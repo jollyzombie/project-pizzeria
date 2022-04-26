aa703b02a5f0447a1e99aa8664c1d62753f35f0e
Ada Jasik <jollyzombie@protonmail.com>
2022-04-26 18:02:26 +0200

    Add price calculation function - processOrder

--- src/js/script.js
+++ src/js/script.js
@@ -53,9 +53,7 @@
   };
 
   const templates = {
-    menuProduct: Handlebars.compile(
-      document.querySelector(select.templateOf.menuProduct).innerHTML // menuProduct method created using the Handlebars library
-    ),
+    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
   class Product {
@@ -65,8 +63,11 @@
       thisProduct.id = id;
       thisProduct.data = data;
 
-      thisProduct.renderInMenu(); // runs method renderInMenu // when a new instance is created for each product, it instantly renders it on the page
+      thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
     }
 
     renderInMenu() {
@@ -85,14 +86,21 @@
       menuContainer.appendChild(thisProduct.element);
     }
 
+    getElements() {
+      const thisProduct = this;
+
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+    }
+
     initAccordion() {
       const thisProduct = this;
 
-      /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-
-      /* START: add event listener to clickable trigger on event click */
-      clickableTrigger.addEventListener('click', function (event) {
+      /*  find the clickable trigger (the element that should react to clicking) & add event listener to clickable trigger on event click */
+      thisProduct.accordionTrigger.addEventListener('click', function (event) {
         event.preventDefault();
 
         /* find active class on product */
@@ -107,11 +115,63 @@
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
     }
+
+    initOrderForm() {
+      const thisProduct = this;
+
+      thisProduct.form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        thisProduct.processOrder(); //callback
+      });
+
+      for (let input of thisProduct.formInputs) {
+        input.addEventListener('change', function () {
+          thisProduct.processOrder(); //callback
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisProduct.processOrder(); // callback
+      });
+    }
+
+    processOrder() {
+      const thisProduct = this;
+
+      const formData = utils.serializeFormToObject(thisProduct.form);
+
+      // set price to default price
+      let price = thisProduct.data.price;
+
+      // for every category (param)
+      for (let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+
+        // for every option in this category
+        for (let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          const selectOption = formData[paramId] && formData[paramId].includes(optionId);
+
+          // check if the option (optionId) of the category (paramId) is selected in the form (formData)
+          if (!option.default && selectOption) {
+            // add option price to price variable
+            price += option.price;
+            // remove option price in price variable
+          } else if (option.default && !selectOption) {
+            price -= option.price;
+          }
+        }
+
+        // update calculated price in HTML
+        thisProduct.priceElem.innerHTML = price;
+      }
+    }
   }
 
-  // object
   const app = {
-    // methods
     initMenu: function () {
       const thisApp = this;
 
@@ -134,12 +194,8 @@
       console.log('settings:', settings);
       console.log('templates:', templates);
 
-      thisApp.initData(); /* initData method provides easy access to data.  */
-      thisApp.initMenu(); /* is triggered after initData because it uses a reference to the data (thisApp.data = dataSource).
-      - iterate over each product
-      - create instances for product in Product class/scheme
-      - constructor runs renderInMenu
-      - renderInMenu creates a DOM that pastes the HTML for the products in thisApp.data.products */
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
